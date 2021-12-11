08517f0fb1eb711c2672bcb9932ffa027576d505
Motus Pilobolus <kayla.prata@gmail.com>
2021-12-11 15:05:52 -0500

    Beginning modifications/customizations

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
